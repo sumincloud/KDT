e83519053ad063d7fdec7218a8e24bff539c8033
sumincloud <sumin.designer@gmail.com>
2024-06-27 18:31:08 +0900

    에스쁘아 추가

---
+++ espior/header.php
@@ -0,0 +1,83 @@
+
+<header>
+    <div>
+      <a href="#" class="icon">
+        <i class="bi bi-list" style="font-size: 30px;"></i>
+      </a>
+      <div style="width:100px; text-align: center;">
+        <a href="./index.php" title="메인페이지로 이동" class="w-100 d-block">
+          <img src="./images/logo_b.png" alt="로고" height="30px">
+        </a>
+      </div>
+      <ul class="status">
+        <li>
+          <a href="#" title="로그인"><i class="bi bi-person"></i></a>
+        </li>
+        <li>
+          <a href="#" title="장바구니"><i class="bi bi-bag"></i></a>
+        </li>
+			</ul>
+    </div>
+
+    <!-- 사이드바 -->
+    <nav id="side_nav open" class="side_nav">
+				<div class="nav_inner clearfix">
+					<div class="nav_header">
+            <a href="#" title="로고">
+              <img src="./images/logo_b.png" alt="로고" height="30px">
+            </a>
+            <a href="#" title="메뉴닫기">메뉴닫기</a>
+						<dl>
+							<dd>
+								<a href="#" title="로그인">로그인</a>
+								<a href="#" title="회원가입">회원가입</a>
+							</dd>
+						</dl>
+					</div>
+					<!-- nav_list -->
+					<ul class="nav_list">
+            <div>
+            <div>
+						<li class="menu">
+							<a href="#" title="ABOUT">ABOUT</a>
+							<div class="depth2">
+								<ul>
+									<li><a href="#" title="BRAND STORY">BRAND STORY</a></li>
+									<li><a href="#" title="BRAND CAMPAIGN">BRAND CAMPAIGN</a></li>
+								</ul>
+							</div>
+						</li>
+						<li class="menu">
+							<a href="#" title="PRODUCT">PRODUCT</a>
+							<div class="depth2">
+								<ul>
+									<li><a href="#" title="NEW&BEST">NEW&BEST</a></li>
+									<li><a href="#" title="FACE">FACE</a></li>
+									<li><a href="#" title="LIP">LIP</a></li>
+									<li><a href="#" title="EYE">EYE</a></li>
+								</ul>
+							</div>
+						</li>
+						<li class="menu">
+							<a href="#" title="PLAY">PLAY</a>
+							<div class="depth2">
+								<ul>
+									<li><a href="#" title="PLAY">PLAY</a></li>
+									<li><a href="#" title="PLAY">PLAY</a></li>
+								</ul>
+							</div>
+						</li>
+						<li class="menu">
+							<a href="#" title="ABOUT">ABOUT</a>
+							<div class="depth2">
+								<ul>
+									<li><a href="#" title="BRAND STORY">BRAND STORY</a></li>
+									<li><a href="#" title="BRAND CAMPAIGN">BRAND CAMPAIGN</a></li>
+								</ul>
+							</div>
+						</li>
+					</div>
+				</div>
+			</nav>
+  </header>
+  
---
+++ espior/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>espior</title>
+  <!-- 부트스트랩 css연결하기 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- 부트스트랩 js연결하기 -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- 부트스트랩 아이콘폰트 연결 -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <!-- 제이쿼리 -->
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <!-- 초기화서식 연결 -->
+  <link rel="stylesheet" href="./css/reset.css">
+  <!-- 공통서식 연결 -->
+  <link rel="stylesheet" href="./css/common.css">
+  <!-- 메인 css 서식 연결 -->
+  <link rel="stylesheet" href="./css/main.css">
+  <!-- 메인 스크립트 -->
+  <script src="./script/main.js"></script>
+</head>
+<body>
+  <!-- 공통헤더삽입 -->
+  <?php include('./header.php')?>
+</body>
+</html>
